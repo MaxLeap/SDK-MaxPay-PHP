87f5691003dcab9e27dd0419305fa442dea10023
heyang <heyang@ilegend.local>
2016-01-05 11:43:49 +0800

    clean MLPay

--- MLPay.php
+++ MLPay.php
@@ -178,30 +178,4 @@ class MLPayApi {
 	}
 	
 }
-/*
-	const CH = 0;
-	const EN = 1;
-	global $LAN;//pred set $LAN
-	$LAN = EN;
-	$data = array();
-	$data["appid"] = "557e5129c10eeabb4878bb38";
-	$data["token"] = "Lm1bckeriwjPy5ECVglXKPdbH7CtPhHljhpWhHr-l5k";
-	//bill
-	$data["billNum"] = "112233";
-	$data["channel"] = "ali_web";
-	$data["totalFee"] = 1;
-	$data["subject"] = "it will be ok!";
-	$data["extras"] = "None";
-	$data["returnUrl"] = "http://www.qq.com";
-	$data["billTimeout"] = 360;
-	$data["showUrl"] = "http://www.qq.com";
-	$data["qrPayMode"] = 0;
-	$result = MLPayApi::bill($data);
-	
-	//records
-	//$data["billNum"] = "112233";
-	//$result = MLPayApi::record($data);
-	$result->msg = getRespErr()[$result->code];
-	print_r($result);
-*/
 ?>

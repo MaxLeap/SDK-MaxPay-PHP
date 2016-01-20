818619946940d05d792b266a98b1b703684d4749
heyang <heyang@ilegend.local>
2016-01-20 11:12:24 +0800

    add we_native

--- testMLpay.php
+++ testMLpay.php
@@ -6,25 +6,28 @@
 	//global $LAN;//pred set $LAN
 	//$LAN = CH;
 	$data = array();
-	$data["appid"] = "568a12086e912100017722e5";
-	$data["token"] = "1eHwAapfyKgRufiBaoKNf63vyuCt_hHlkDdGncfGhBs";
 	//bill
 	$data["billNum"] = "112233";
-	$data["channel"] = "ali_web";
-	$data["totalFee"] = 1;
-	$data["subject"] = "it will be ok!";
-	$arrayName = array('a' => 1, 'b' => 2);
-	$data["extras"] = $arrayName;
-	$data["returnUrl"] = "http://www.qq.com";
-	$data["billTimeout"] = 360;
-	//$data["showUrl"] = "http://www.qq.com";
-	//$data["qrPayMode"] = 0;
-	$result = MLPayApi::bill($data);
-
-	//records
-	//$data["billNum"] = "112233";
-	//$result = MLPayApi::record($data);
-	$result->msg = getRespErr()[$result->code];
+	$data["appid"] = "568a12086e912100017722e5";
+        $data["token"] = "1eHwAapfyKgRufiBaoKNf63vyuCt_hHlkDdGncfGhBs";
+	if (isset($argv[1]) && $argv[1] == "bill"){
+		$data["channel"] = $argv[2]?$argv[2]:null;
+		$data["totalFee"] = 1;
+		$data["subject"] = "it will be ok!";
+		$arrayName = array('a' => 1, 'b' => 2);
+		$data["extras"] = $arrayName;
+		$data["returnUrl"] = "http://www.qq.com";
+		$data["billTimeout"] = 360;
+		//$data["showUrl"] = "http://www.qq.com";
+		//$data["qrPayMode"] = 0;
+		$result = MLPayApi::bill($data);
+	}elseif(isset($argv[1]) && $argv[1] == "record"){
+		//records
+		$result = MLPayApi::record($data);
+		$result->msg = getRespErr()[$result->code];
+	}else{
+		die("no way celect");
+	}
 	print_r($result);
 
 ?>
